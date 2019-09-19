616c98f23018ca3567725b6659fac09053935d85
Nick <cloud@gmail.com>
2019-09-19 13:35:52 -0500

    Completed. Rotation works, code commented.

--- app/src/main/java/android/bignerdranch/surveyapp/MainActivity.java
+++ app/src/main/java/android/bignerdranch/surveyapp/MainActivity.java
@@ -3,15 +3,17 @@ package android.bignerdranch.surveyapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String KEY_INDEX = "vote";
+    // Keys strings for finding saved instance data
+    private static final String KEY_INDEX_YES = "yes vote";
+    private static final String KEY_INDEX_NO = "no vote";
 
+    // Initialized widgets to be inflated
     Button mYesButton;
     Button mNoButton;
     TextView mSurveyQuestion;
@@ -19,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     TextView mNoCount;
     Button mResetVotes;
 
+    // Initialize variables for counting/resetting votes
     private int mVoteYesCount = 0;
     private int mVoteNoCount = 0;
     private int resetVotes = 0;
@@ -28,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Calls references to String resources
         mYesButton = findViewById(R.id.yes_button);
         mNoButton = findViewById(R.id.no_button);
         mSurveyQuestion = findViewById(R.id.survey_question);
@@ -35,20 +39,19 @@ public class MainActivity extends AppCompatActivity {
         mNoCount = findViewById(R.id.no_count);
         mResetVotes = findViewById(R.id.reset_button);
 
+        // Checks for saved instance variable data during rotation
         if (savedInstanceState != null) {
-            mVoteYesCount = savedInstanceState.getInt(KEY_INDEX, 0);
-            mVoteNoCount = savedInstanceState.getInt(KEY_INDEX, 0);
-//            mYesCount.setText(""+mVoteYesCount);
-//            mNoCount.setText(""+mVoteNoCount);
-
+            mVoteYesCount = savedInstanceState.getInt(KEY_INDEX_YES, mVoteYesCount);
+            mVoteNoCount = savedInstanceState.getInt(KEY_INDEX_NO, mVoteNoCount);
+            mYesCount.setText(String.valueOf(mVoteYesCount));
+            mNoCount.setText(String.valueOf(mVoteNoCount));
         }
 
+        // Button listeners
         mYesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 addVote();
-//                mYesCount.setText(""+mVoteYesCount);
-//                mVoteYesCount++;
             }
         });
 
@@ -56,42 +59,38 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 addVote();
-//                mNoCount.setText(""+mVoteNoCount);
-//                mVoteNoCount++;
             }
         });
 
         mResetVotes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mYesCount.setText(""+resetVotes);
-                mNoCount.setText(""+resetVotes);
-                mVoteYesCount = 0;
+                mYesCount.setText(String.valueOf(resetVotes));
+                mNoCount.setText(String.valueOf(resetVotes));
+                mVoteYesCount = 0; // sets counts back to 0
                 mVoteNoCount = 0;
             }
         });
-
-//        mYesCount.setText(mVoteYesCount);
-//        mNoCount.setText((mVoteNoCount));
     }
 
-//    @Override
-//    public void onSaveInstanceState(Bundle savedInstanceState) {
-//        super.onSaveInstanceState(savedInstanceState);
-//        savedInstanceState.putInt(KEY_INDEX, mVoteYesCount);
-//        mYesCount.setText(""+mVoteYesCount);
-//    }
+    // Method to save instance variable data in Bundle
+    @Override
+    public void onSaveInstanceState(Bundle outBundle) {
+        super.onSaveInstanceState(outBundle);
+        outBundle.putInt(KEY_INDEX_YES, mVoteYesCount);
+        outBundle.putInt(KEY_INDEX_NO, mVoteNoCount);
+    }
 
+    // Adds single vote per press to corresponding yes or no button
     private void addVote() {
 
         if (mYesButton.isPressed()) {
-            mVoteYesCount++;
-            mYesCount.setText(""+mVoteYesCount);
+            mVoteYesCount++; // +1 vote
+            mYesCount.setText(String.valueOf(mVoteYesCount)); // Sets value to textView
         }
         else if (mNoButton.isPressed()) {
             mVoteNoCount++;
-            mNoCount.setText(""+mVoteNoCount);
-
+            mNoCount.setText(String.valueOf(mVoteNoCount));
         }
     }
 }
